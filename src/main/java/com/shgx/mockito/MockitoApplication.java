07093a970e8cc1c8c0687710ee956557ec40e73d
guangxush <guangxush@163.com>
2020-09-28 23:04:52 +0800

    init

--- src/main/java/com/shgx/mockito/MockitoApplication.java
+++ src/main/java/com/shgx/mockito/MockitoApplication.java
@@ -2,8 +2,10 @@ package com.shgx.mockito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:META-INF/spring/spring.xml"})
 public class MockitoApplication {
 
     public static void main(String[] args) {
